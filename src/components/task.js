0e955b422c7fa948a264eaf6835f50e88de64ba9
maffluk <maffluk@gmail.com>
2019-07-11 20:42:26 +0300

    Update task.js

--- src/components/task.js
+++ src/components/task.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Task = ({ title, isCompleat, index, click }) => {
     return (
         <React.Fragment >
-            <input type="checkbox" name="CheckBoxInputName" defaultChecked={isCompleat} value={title} id={`CheckBox${index}`} onClick={() => click(title)}/>
+            <input type="checkbox" name="CheckBoxInputName" defaultChecked={isCompleat} value={title} id={`CheckBox${index}`} onClick={() => click(title)} />
             <label className="list-group-item" htmlFor={`CheckBox${index}`}>{title}</label>
         </React.Fragment>
     )
