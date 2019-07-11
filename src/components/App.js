639fa4e6c0ef5b2184f0c36e29a7707e7261287e
maffluk <maffluk@gmail.com>
2019-07-11 19:28:19 +0300

    Add functional

--- src/components/App.js
+++ src/components/App.js
@@ -4,29 +4,47 @@ import Task from './task'
 
 function App() {
   const [tasks, addTask] = useState([{ title: 'Купить хлеб', isCompleat: true }, { title: 'Изучить хуки', isCompleat: false }])
+  const [appState, setState] = useState('All')
+
   let inputTask
+
   const addSomeTask = () => {
     if (inputTask.value === '') return null
-    let newtate = [...tasks]
-    newtate.push({ title: inputTask.value, isCompleat: false })
+    let newState = [...tasks]
+    newState.push({ title: inputTask.value, isCompleat: false })
     inputTask.value = ''
-    addTask(newtate)
+    addTask(newState)
   }
+
+  const compleatTask = title => {
+    let newState = [...tasks]
+    newState.forEach(item => {
+      if(item.title === title){
+        item.isCompleat = !item.isCompleat
+      }
+    })
+    addTask(newState)
+  }
+
   return (
     <div className="App">
       <header>
         <input className="form-control form-control-lg" type="text" placeholder='Write your task' ref={(node) => inputTask = node} />
-
         <button type="button" onClick={addSomeTask} className="btn btn-primary btn-lg">Add</button>
-
-        <button type="button" className="btn btn-outline-success switch">All</button>
-        <button type="button" className="btn btn-outline-danger">Incompleat</button>
+        <button type="button" onClick={() => setState('All')} className={`btn btn-outline-success switch ${appState === 'All' ? 'active' : null}`}>All</button>
+        <button type="button" onClick={() => setState('Incompleat')} className={`btn btn-outline-danger ${appState === 'Incompleat' ? 'active' : null}`}>Incompleat</button>
       </header>
 
       <div className="list-group">
-        {tasks.map(({ title, isCompleat }, index) => <Task key={title} title={title} index={index} isCompleat={isCompleat} />)}
+        {tasks.map(({ title, isCompleat }, index) => {
+          if (appState === 'Incompleat' && isCompleat === false) {
+            return <Task key={title} title={title} index={index} click={compleatTask} isCompleat={isCompleat} />
+          }
+          else if (appState === 'All') {
+            return <Task key={title} title={title} index={index} click={compleatTask} isCompleat={isCompleat} />
+          }
+        })}
       </div>
-
     </div>
   );
 }
--- src/components/task.js
+++ src/components/task.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Task = ({ title, isCompleat, index }) => {
+const Task = ({ title, isCompleat, index, click }) => {
     return (
-        <React.Fragment>
-            <input type="checkbox" name="CheckBoxInputName" defaultChecked={isCompleat} value={title} id={`CheckBox${index}`} />
+        <React.Fragment >
+            <input type="checkbox" name="CheckBoxInputName" defaultChecked={isCompleat} value={title} id={`CheckBox${index}`} onClick={() => click(title)}/>
             <label className="list-group-item" htmlFor={`CheckBox${index}`}>{title}</label>
         </React.Fragment>
     )
