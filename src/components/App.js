1a41107d9f83cd4c2f4fc98ca82c1a963b6b7dd0
maffluk <maffluk@gmail.com>
2019-07-13 13:03:14 +0300

    Drag and Drop

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React, { useState } from 'react'
 import '../App.css'
+import { DragDropContext, Droppable } from 'react-beautiful-dnd'
 import Task from './task'
 
+const getItemStyle = (isDragging, draggableStyle) => ({
+  userSelect: "none",
+  background: isDragging ? "lightgreen" : "grey",
+  ...draggableStyle
+});
+
+
 function App() {
-  const [tasks, addTask] = useState([{ title: 'Купить хлеб', isCompleat: true }, { title: 'Изучить хуки', isCompleat: false }])
+  const [tasks, addTask] = useState([{ id: "item-0", title: 'Купить хлеб', isCompleat: true }, { id: "item-1", title: 'Изучить хуки', isCompleat: false }])
   const [appState, setState] = useState('All')
 
   let inputTask
@@ -11,7 +19,7 @@ function App() {
   const addSomeTask = () => {
     if (inputTask.value === '') return null
     let newState = [...tasks]
-    newState.push({ title: inputTask.value, isCompleat: false })
+    newState.push({ title: inputTask.value, isCompleat: false, id: `item-${newState.length}` })
     inputTask.value = ''
     addTask(newState)
   }
@@ -19,33 +27,52 @@ function App() {
   const compleatTask = title => {
     let newState = [...tasks]
     newState.forEach(item => {
-      if(item.title === title){
+      if (item.title === title) {
         item.isCompleat = !item.isCompleat
       }
     })
     addTask(newState)
   }
+  const onDragEnd = result => {
+    if (!result.destination) {
+      return
+    }
+    let newState = [...tasks]
+    let [removed] = newState.splice(result.source.index, 1);
+    newState.splice(result.destination.index, 0, removed);
 
+    addTask(newState)
+  }
   return (
+    <DragDropContext onDragEnd={onDragEnd}>
     <div className="App">
       <header>
         <input className="form-control form-control-lg" type="text" placeholder='Write your task' ref={(node) => inputTask = node} />
         <button type="button" onClick={addSomeTask} className="btn btn-primary btn-lg">Add</button>
         <button type="button" onClick={() => setState('All')} className={`btn btn-outline-success switch ${appState === 'All' ? 'active' : null}`}>All</button>
-        <button type="button" onClick={() => setState('Incompleat')} className={`btn btn-outline-danger ${appState === 'Incompleat' ? 'active' : null}`}>Incompleat</button>
+        <button type="button" onClick={() => setState('Incompleat')} className={`btn btn-outline-danger switch ${appState === 'Incompleat' ? 'active' : null}`}>Incompleat</button>
       </header>
-
       <div className="list-group">
-        {tasks.map(({ title, isCompleat }, index) => {
-          if (appState === 'Incompleat' && isCompleat === false) {
-            return <Task key={title} title={title} index={index} click={compleatTask} isCompleat={isCompleat} />
-          }
-          else if (appState === 'All') {
-            return <Task key={title} title={title} index={index} click={compleatTask} isCompleat={isCompleat} />
-          }
-        })}
+          <Droppable droppableId='droppableId'>
+            {(provided) => {
+              return (
+                <div ref={provided.innerRef} {...provided.droppableProps}>
+                  {tasks.map(({ title, isCompleat, id }, index) => {
+                    if (appState === 'Incompleat' && isCompleat === false) {
+                      return <Task key={title} title={title} id={id} index={index} click={compleatTask} isCompleat={isCompleat} />
+                    }
+                    else if (appState === 'All') {
+                      return <Task key={title} title={title} id={id} index={index} click={compleatTask} isCompleat={isCompleat} />
+                    }
+                  })}
+                  {provided.placeholder}
+                </div>
+              )
+            }}
+          </Droppable>
       </div>
     </div>
+    </DragDropContext>
   );
 }
 
--- src/components/task.js
+++ src/components/task.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import { Draggable } from 'react-beautiful-dnd'
 
-const Task = ({ title, isCompleat, index, click }) => {
+const Task = ({ title, isCompleat, id, click, index}) => {
     return (
-        <React.Fragment >
-            <input type="checkbox" name="CheckBoxInputName" defaultChecked={isCompleat} value={title} id={`CheckBox${index}`} onClick={() => click(title)} />
-            <label className="list-group-item" htmlFor={`CheckBox${index}`}>{title}</label>
-        </React.Fragment>
+        <Draggable draggableId={id} index={index}>
+            {(provided, snapshot) => (
+                <div className="custom-control custom-checkbox task" {...provided.draggableProps}
+                {...provided.dragHandleProps}
+                ref={provided.innerRef}
+                isDragging={snapshot.isDragging} >
+                    <input type="checkbox" defaultChecked={isCompleat} onClick={() => click(title)} className="custom-control-input" id={`customCheck${index}`} />
+                    <label className="custom-control-label" htmlFor={`customCheck${index}`}>{title}</label>
+                </div>
+            )}
+        </Draggable>
     )
 }
 
